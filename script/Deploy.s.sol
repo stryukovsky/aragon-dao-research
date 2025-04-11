b52ffd1f4e3dfef3d5cf77266855d30b97620548
Jør∂¡ <itsbrickpop@gmail.com>
2025-04-11 17:03:36 +0200

    Stage 1: deploying the static contracts on the local script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,21 +1,31 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.17;
 
 import {Script, console} from "forge-std/Script.sol";
+import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
 import {DAO} from "@aragon/osx/core/dao/DAO.sol";
-import {DAOFactory} from "@aragon/osx/framework/dao/DAOFactory.sol";
+import {IDAO} from "@aragon/osx-commons-contracts/src/dao/IDAO.sol";
 import {DAORegistry} from "@aragon/osx/framework/dao/DAORegistry.sol";
 import {PluginRepo} from "@aragon/osx/framework/plugin/repo/PluginRepo.sol";
-import {PluginRepoFactory} from "@aragon/osx/framework/plugin/repo/PluginRepoFactory.sol";
 import {PluginRepoRegistry} from "@aragon/osx/framework/plugin/repo/PluginRepoRegistry.sol";
 import {PlaceholderSetup} from "@aragon/osx/framework/plugin/repo/placeholder/PlaceholderSetup.sol";
-import {PluginSetupProcessor} from "@aragon/osx/framework/plugin/setup/PluginSetupProcessor.sol";
 import {ENSSubdomainRegistrar} from "@aragon/osx/framework/utils/ens/ENSSubdomainRegistrar.sol";
 import {Executor as GlobalExecutor} from "@aragon/osx-commons-contracts/src/executors/Executor.sol";
 
-/// @notice Performs a full deploy of OSx, along with the core Aragon plugins and the Management DAO
-/// @dev Given that deployong all the contracts within the factory would hit the gas limit, the script does the deployment in two stages:
-/// @dev 1) Deploy the raw contracts and store their address locally
+import {AdminSetup} from "@aragon/admin-plugin/AdminSetup.sol";
+import {MultisigSetup} from "@aragon/multisig-plugin/MultisigSetup.sol";
+import {TokenVotingSetup} from "@aragon/token-voting-plugin/TokenVotingSetup.sol";
+import {GovernanceERC20} from "@aragon/token-voting-plugin/ERC20/governance/GovernanceERC20.sol";
+import {GovernanceWrappedERC20} from "@aragon/token-voting-plugin/ERC20/governance/GovernanceWrappedERC20.sol";
+import {StagedProposalProcessorSetup} from "@aragon/staged-proposal-processor-plugin/StagedProposalProcessorSetup.sol";
+
+import {ProtocolFactory} from "../src/ProtocolFactory.sol";
+
+/// @notice This local script triggers a full deploy of OSx, along with the core Aragon plugins and the Management DAO
+/// @dev No privileged actions are performed within this file. All of them take place within the ProtocolFactory contract, on-chain.
+/// @dev Given that deploying the factory with all contracts embedded would hit the gas limit, the deployment has two stages:
+/// @dev 1) Deploy the raw contracts and store their addresses locally (this file)
 /// @dev 2) Deploy the factory with the addresses above and tell it to orchestrate the protocol deployment
 contract DeployScript is Script {
     DAO dao;
@@ -25,6 +35,13 @@ contract DeployScript is Script {
     PlaceholderSetup placeholderSetup;
     ENSSubdomainRegistrar ensSubdomainRegistrar;
     GlobalExecutor globalExecutor;
+
+    AdminSetup adminSetup;
+    MultisigSetup multisigSetup;
+    TokenVotingSetup tokenVotingSetup;
+    StagedProposalProcessorSetup stagedProposalProcessorSetup;
+
+    ProtocolFactory factory;
 
     modifier broadcast() {
         uint256 privKey = vm.envUint("DEPLOYMENT_PRIVATE_KEY");
@@ -40,12 +57,20 @@ contract DeployScript is Script {
 
     function run() public broadcast {
         deployOSxImplementations();
+        deployAdminSetup();
+        deployMultisigSetup();
+        deployTokenVotingSetup();
+        deployStagedProposalProcessorSetup();
+
+        // Factory call
+        factory = new ProtocolFactory(getFactoryParams());
+        factory.deployOnce();
     }
 
     // Internal helpers
 
     function deployOSxImplementations() internal {
-        /// @dev Deploy an implementation with empty values
+        /// @dev Deploy implementations with empty values. They will be used to create proxies.
         dao = new DAO();
         daoRegistry = new DAORegistry();
         pluginRepo = new PluginRepo();
@@ -53,15 +78,56 @@ contract DeployScript is Script {
         placeholderSetup = new PlaceholderSetup();
         ensSubdomainRegistrar = new ENSSubdomainRegistrar();
         globalExecutor = new GlobalExecutor();
+
+        /// @dev The DAOFactory, PluginRepoFactory and PluginSetupProcessor are static.
+        /// @dev These contracts will be deployed by the Protocol Factory.
     }
 
-    function deployAdminPluginContracts() internal {}
+    function deployAdminSetup() internal {
+        adminSetup = new AdminSetup();
+    }
 
-    function deployMultisigPluginContracts() internal {}
+    function deployMultisigSetup() internal {
+        multisigSetup = new MultisigSetup();
+    }
 
-    function deployTokenVotingPluginContracts() internal {}
+    function deployTokenVotingSetup() internal {
+        tokenVotingSetup = new TokenVotingSetup(
+            new GovernanceERC20(
+                IDAO(address(0)),
+                "",
+                "",
+                GovernanceERC20.MintSettings(new address[](0), new uint256[](0))
+            ),
+            new GovernanceWrappedERC20(IERC20Upgradeable(address(0)), "", "")
+        );
+    }
 
-    function deployStagedProposalProcessorPluginContracts() internal {}
+    function deployStagedProposalProcessorSetup() internal {
+        stagedProposalProcessorSetup = new StagedProposalProcessorSetup();
+    }
 
-    function getFactoryParams() internal {}
+    function getFactoryParams()
+        internal
+        view
+        returns (ProtocolFactory.DeploymentParameters memory params)
+    {
+        params = ProtocolFactory.DeploymentParameters({
+            osxImplementations: ProtocolFactory.OSxImplementations({
+                dao: dao,
+                daoRegistry: daoRegistry,
+                pluginRepo: pluginRepo,
+                pluginRepoRegistry: pluginRepoRegistry,
+                placeholderSetup: placeholderSetup,
+                ensSubdomainRegistrar: ensSubdomainRegistrar,
+                globalExecutor: globalExecutor
+            }),
+            pluginSetups: ProtocolFactory.PluginSetups({
+                adminSetup: adminSetup,
+                multisigSetup: multisigSetup,
+                tokenVotingSetup: tokenVotingSetup,
+                stagedProposalProcessorSetup: stagedProposalProcessorSetup
+            })
+        });
+    }
 }
---
+++ src/ProtocolFactory.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import {console} from "forge-std/Script.sol";
+import {DAO} from "@aragon/osx/core/dao/DAO.sol";
+import {DAOFactory} from "@aragon/osx/framework/dao/DAOFactory.sol";
+import {DAORegistry} from "@aragon/osx/framework/dao/DAORegistry.sol";
+import {PluginRepo} from "@aragon/osx/framework/plugin/repo/PluginRepo.sol";
+import {PluginRepoFactory} from "@aragon/osx/framework/plugin/repo/PluginRepoFactory.sol";
+import {PluginRepoRegistry} from "@aragon/osx/framework/plugin/repo/PluginRepoRegistry.sol";
+import {PlaceholderSetup} from "@aragon/osx/framework/plugin/repo/placeholder/PlaceholderSetup.sol";
+import {PluginSetupProcessor} from "@aragon/osx/framework/plugin/setup/PluginSetupProcessor.sol";
+import {ENSSubdomainRegistrar} from "@aragon/osx/framework/utils/ens/ENSSubdomainRegistrar.sol";
+import {Executor as GlobalExecutor} from "@aragon/osx-commons-contracts/src/executors/Executor.sol";
+
+import {AdminSetup} from "@aragon/admin-plugin/AdminSetup.sol";
+import {MultisigSetup} from "@aragon/multisig-plugin/MultisigSetup.sol";
+import {TokenVotingSetup} from "@aragon/token-voting-plugin/TokenVotingSetup.sol";
+import {StagedProposalProcessorSetup} from "@aragon/staged-proposal-processor-plugin/StagedProposalProcessorSetup.sol";
+
+/// @notice This contract orchestrates the full protocol deployment, including the Managing DAO, OSx and Aragon's core plugins.
+/// @dev Given that deploying the factory with all contracts embedded would hit the gas limit, the deployment has two stages:
+/// @dev 1) Deploy the raw contracts and store their addresses locally
+/// @dev 2) Deploy the factory with the addresses above and tell it to orchestrate the protocol deployment (this file)
+contract ProtocolFactory {
+    /// @notice The struct containing all the parameters to deploy the protocol
+    struct DeploymentParameters {
+        OSxImplementations osxImplementations;
+        PluginSetups pluginSetups;
+    }
+
+    /// @notice The struct containing the implementation addresses for OSx
+    struct OSxImplementations {
+        DAO dao;
+        DAORegistry daoRegistry;
+        PluginRepo pluginRepo;
+        PluginRepoRegistry pluginRepoRegistry;
+        PlaceholderSetup placeholderSetup;
+        ENSSubdomainRegistrar ensSubdomainRegistrar;
+        GlobalExecutor globalExecutor;
+    }
+
+    struct PluginSetups {
+        AdminSetup adminSetup;
+        MultisigSetup multisigSetup;
+        TokenVotingSetup tokenVotingSetup;
+        StagedProposalProcessorSetup stagedProposalProcessorSetup;
+    }
+
+    /// @notice The struct containing the deployed protocol addresses
+    struct Deployment {
+        // OSx implementations
+        DAO dao;
+        DAORegistry daoRegistry;
+        PluginRepo pluginRepo;
+        PluginRepoRegistry pluginRepoRegistry;
+        PlaceholderSetup placeholderSetup;
+        ENSSubdomainRegistrar ensSubdomainRegistrar;
+        GlobalExecutor globalExecutor;
+        // OSx static contracts
+        DAOFactory daoFactory;
+        PluginRepoFactory pluginRepoFactory;
+        PluginSetupProcessor pluginSetupProcessor;
+    }
+
+    /// @notice Thrown when attempting to call deployOnce() when the protocol is already deployed.
+    error AlreadyDeployed();
+
+    DeploymentParameters public parameters;
+    Deployment public deployment;
+
+    /// @notice Initializes the factory and performs the full deployment. Values become read-only after that.
+    /// @param _parameters The parameters of the one-time deployment.
+    constructor(DeploymentParameters memory _parameters) {
+        parameters = _parameters;
+    }
+
+    function deployOnce() external {
+        if (address(deployment.dao) != address(0)) revert AlreadyDeployed();
+
+        // TODO
+    }
+
+    // Internal helpers
+
+    function prepareManagementDao() internal {}
+
+    function prepareOSx() internal {
+        deployment.daoFactory = new DAOFactory(
+            DAORegistry(address(0)),
+            PluginSetupProcessor(address(0))
+        );
+        deployment.pluginRepoFactory = new PluginRepoFactory(
+            PluginRepoRegistry(address(0))
+        );
+        deployment.pluginSetupProcessor = new PluginSetupProcessor(
+            PluginRepoRegistry(address(0))
+        );
+    }
+}
